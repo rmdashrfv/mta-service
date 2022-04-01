2227090d7740a630a08878d83776f3ebe695b717
Michael Law <michaellaw@gmx.com>
2022-04-01 17:18:49 -0400

    Load arrival times dynamically

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ const getAllStations = async () => {
     // console.log('Station', element)
     let h2 = document.createElement('h2')
     h2.innerText = element.name 
+    h2.dataset.stationId = element.id
+    h2.addEventListener('click', () => {
+       getArrivalTimes(h2.dataset.stationId) 
+    })
     div.append(h2)
   })
+}
+
+const getArrivalTimes = async (stationId) => {
+  let req = await fetch(`https://mtaapi.herokuapp.com/api?id=${stationId}`)
+  let res = await req.json()
+  console.log('Arrival times', res.result.arrivals)
 }
 
 getAllStations()
